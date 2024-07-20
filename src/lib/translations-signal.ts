dfa94fa204b9da6b438d2c5f89ef079662fe9182
Marc Schmidt <code@masch212.de>
2024-07-20 15:27:01 +0200

    feat: add ability to retrieve key by unspecific string (#1)
    * chore: update packages
    
    * feat: add ability to retrieve key by unspecific string
    
    * docs: add documentation for _unsafe property

--- src/lib/translations-signal.ts
+++ src/lib/translations-signal.ts
@@ -1,7 +1,7 @@
 import { computed, Signal } from '@angular/core';
 
-import { TranslationsSignal } from './types';
-import { getDeepValue, isFunctionKey } from './utils';
+import { TranslationSignal, TranslationsSignal } from './types';
+import { getDeepValue } from './utils';
 
 export function toTranslationsSignal<
   T extends Record<string, unknown>,
@@ -9,11 +9,25 @@ export function toTranslationsSignal<
 >(signal: Signal<T | undefined>, separator: TSeparator): TranslationsSignal<T, TSeparator> {
   return new Proxy(signal, {
     get(target: Signal<T | undefined> & Record<string | symbol, unknown>, prop) {
-      if (typeof prop !== 'string' || isFunctionKey(prop)) {
+      if (prop in target) {
         return target[prop];
       }
-      const sig = computed(() => getDeepValue(target(), prop.split(separator)));
-      Object.defineProperty(sig, 'key', { value: prop });
+
+      if (typeof prop !== 'string') {
+        return undefined;
+      }
+
+      if (prop === '_unsafe') {
+        const unsafe = new Proxy(target, this);
+        Object.defineProperty(unsafe, '_unsafe', { value: unsafe });
+        return unsafe;
+      }
+
+      const valueSig = computed(() => String(getDeepValue(target(), prop.split(separator)) ?? ''));
+      const sig: TranslationSignal = Object.assign(valueSig, { key: prop });
+
+      Object.defineProperty(target, prop, { value: sig });
+
       return sig;
     },
   }) as TranslationsSignal<T, TSeparator>;
